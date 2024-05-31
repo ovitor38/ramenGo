a363102d1e72951fb1743b098e049652a3af55b8
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-05-31 15:44:27 -0300

    refactor(messages): remove unnecessary messages response

--- src/helpers/response/response-messages/messages.ts
+++ src/helpers/response/response-messages/messages.ts
@@ -9,12 +9,6 @@ export enum methodType {
   DELETED = 'deleted'
 }
 
-// USERS
-export const USER_CREATED_SUCESSFULLY = 'User created successfuly'
-export const USER_UPDATED_SUCESSFULLY = 'User updated successfuly'
-export const USER_DELETED_SUCESSFULLY = 'User deleted successfuly'
-
-// BROTH
 export const genericSuccessfulyMessage = (
   entity: entityType,
   method: methodType
